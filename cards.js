6a08d41104eaa3421e17a48fe40a9474eaeda14d
quewon <quewon.chin@gmail.com>
2021-04-22 16:53:46 +0900

    .

---
+++ cards.js
@@ -0,0 +1,168 @@
+var suits = ["spades", "diamonds", "clubs", "hearts"];
+var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+var card_settings = {};
+
+class Deck {
+  constructor(func) {
+    this.deck = [];
+
+    for (let i = 0; i < suits.length; i++) {
+      for (let ii=0; ii < values.length; ii++) {
+        let card = new Card(suits[i], values[ii]);
+        this.deck.push(card);
+      }
+    }
+
+    this.shuffle();
+
+    for (let card in this.deck) {
+      this.deck[card].index = card;
+      this.deck[card].createCanvas();
+      tabletop.zindex++;
+    }
+  }
+  shuffle() {
+    for (let i = 0; i < this.deck.length * 20; i++)
+  	{
+  		const a = Math.floor((Math.random() * this.deck.length));
+  		const b = Math.floor((Math.random() * this.deck.length));
+  		const aa = this.deck[a];
+
+  		this.deck[a] = this.deck[b];
+  		this.deck[b] = aa;
+  	}
+  }
+}
+
+class Card {
+  constructor(suit, value) {
+    this.suit = suit;
+    this.value = value;
+    this.face = "down";
+    this.index = undefined;
+
+    this.pos = [0, 0, 0, 0];
+  }
+  createCanvas() {
+    let canvas = document.createElement("canvas");
+    canvas.className = "card";
+    canvas.width = 100;
+    canvas.height = 150;
+    canvas.id = this.suit + " " + this.value;
+    canvas.style.zIndex = this.index;
+    this.canvas = canvas;
+    table.appendChild(canvas);
+
+    let context = canvas.getContext("2d");
+    this.context = context;
+    this.context.drawImage(imgs["img/cardback.png"], 0, 0);
+
+    canvas.addEventListener('contextmenu', function(e) {
+      e.preventDefault();
+
+      const card = tabletop.cards[this.id];
+
+      if (card.face == "down") {
+        // TODO: check if card index is highest among cards that havent been flipped over
+        // if not, player gets called out for potentially cheating
+
+        card.face = "up";
+        card.context.clearRect(0, 0, 100, 150);
+
+        card.drawCard();
+      } else {
+        card.face = "down";
+        card.context.clearRect(0, 0, 100, 150);
+        card.context.drawImage(imgs["img/cardback.png"], 0, 0);
+      }
+
+      card.bringToFront();
+
+      return false
+    }, false);
+
+    canvas.onmousedown = function(e) {
+      e = e || window.event;
+      e.preventDefault();
+      const card = tabletop.cards[this.id];
+
+      card.pos[2] = e.clientX;
+      card.pos[3] = e.clientY;
+
+      document.onmouseup = function() {
+        //TODO: if you let go of a card at the edge of the screen and the card is more than halfway in the edge, someone will toss it back to u and be like "bro dont go losing my cards this is my dad's deck" or something 
+
+        document.onmouseup = null;
+        document.onmousemove = null;
+      };
+      document.onmousemove = function(e) {
+        e = e || window.event;
+        const card = tabletop.cards[canvas.id];
+
+        card.pos[0] = card.pos[2] - e.clientX;
+        card.pos[1] = card.pos[3] - e.clientY;
+        card.pos[2] = e.clientX;
+        card.pos[3] = e.clientY;
+
+        canvas.style.top = (canvas.offsetTop - card.pos[1] + "px");
+        canvas.style.left = (canvas.offsetLeft - card.pos[0] + "px");
+      };
+
+      card.bringToFront();
+    }
+
+    tabletop.cards[canvas.id] = this;
+  }
+  bringToFront() {
+    tabletop.zindex++;
+    this.canvas.style.zIndex = tabletop.zindex;
+  }
+  drawCard() {
+    const ctx = this.context;
+    const l = card_settings;
+
+    ctx.drawImage(imgs["img/card.png"], 0, 0);
+
+    // draw small suits
+    let small = l.small[this.suit].up;
+    ctx.drawImage(small.img, small.sx, small.sy, small.sWidth, small.sHeight, l.suitsmall[0], l.suitsmall[1], small.sWidth, small.sHeight);
+
+    small = l.small[this.suit].down;
+    ctx.drawImage(small.img, small.sx, small.sy, small.sWidth, small.sHeight, 100-l.suitsmall[0]-small.sWidth, 150-l.suitsmall[1]-small.sHeight, small.sWidth, small.sHeight);
+
+    // draw values
+    const value = l.font[this.value];
+    small = value.up;
+    let offset = 0;
+    if (small.sWidth < 10) offset = 2;
+
+    ctx.drawImage(small.img, small.sx, small.sy, small.sWidth, small.sHeight, l.value[0]+offset, l.value[1], small.sWidth, small.sHeight);
+
+    small = value.down;
+    ctx.drawImage(small.img, small.sx, small.sy, small.sWidth, small.sHeight, 100-l.value[0]-small.sWidth-offset, 150-l.value[1]-small.sHeight, small.sWidth, small.sHeight);
+
+    // lay out centerpiece
+    let layout = l.layout[this.value];
+    let valueIsNum = false;
+
+    if (/\d/.test(this.value)) valueIsNum = true;
+
+    if (valueIsNum) {
+      let big = l.big[this.suit].up;
+      for (const coords in layout.up) {
+        let c = layout.up[coords];
+        ctx.drawImage(big.img, big.sx, big.sy, big.sWidth, big.sHeight, c[0], c[1], big.sWidth, big.sHeight);
+      }
+
+      big = l.big[this.suit].down;
+      for (const coords in layout.down) {
+        let c = layout.down[coords];
+        ctx.drawImage(big.img, big.sx, big.sy, big.sWidth, big.sHeight, c[0], c[1], big.sWidth, big.sHeight);
+      }
+    } else {
+      let big = l.big[this.suit][this.value];
+      console.log(layout);
+      ctx.drawImage(big.img, big.sx, big.sy, big.sWidth, big.sHeight, layout[0], layout[1], big.sWidth, big.sHeight);
+    }
+  }
+}
---
+++ main.js
@@ -0,0 +1,552 @@
+var table = document.getElementById("tabletop");
+var tabletop;
+
+var images = [
+  "img/big_suits.png",
+  "img/card_font.png",
+  "img/card.png",
+  "img/cardback.png",
+  "img/royals.png",
+  "img/small_suits.png",
+];
+var imgs = {};
+
+init();
+
+function init() {
+  // load images
+  const length = images.length;
+  let tally = 0;
+
+  for (const i of images) {
+    let img = new Image();
+    img.onload = function() {
+      imgs[i] = img;
+      tally++;
+      console.log(tally+"/"+length+" images loaded");
+      if (tally == images.length) {
+        console.log("all images loaded");
+        init_2()
+      }
+    }
+    img.src = i;
+    img.setAttribute('crossOrigin', '');
+  }
+
+  function init_2() {
+    card_settings = {
+      //TODO
+      value: [6, 6],
+      suitsmall: [5, 14],
+
+      font: {
+        "10": {
+          up: {
+            img: imgs["img/card_font.png"],
+            sx: 0,
+            sy: 0,
+            sWidth: 10,
+            sHeight: 7,
+          },
+          down: {
+            img: imgs["img/card_font.png"],
+            sx: 0,
+            sy: 7,
+            sWidth: 10,
+            sHeight: 7,
+          }
+        },
+        "2": {
+          up: {
+            img: imgs["img/card_font.png"],
+            sx: 10,
+            sy: 0,
+            sWidth: 5,
+            sHeight: 7,
+          },
+          down: {
+            img: imgs["img/card_font.png"],
+            sx: 10,
+            sy: 7,
+            sWidth: 5,
+            sHeight: 7,
+          }
+        },
+        "3": {
+          up: {
+            img: imgs["img/card_font.png"],
+            sx: 15,
+            sy: 0,
+            sWidth: 5,
+            sHeight: 7,
+          },
+          down: {
+            img: imgs["img/card_font.png"],
+            sx: 15,
+            sy: 7,
+            sWidth: 5,
+            sHeight: 7,
+          }
+        },
+        "4": {
+          up: {
+            img: imgs["img/card_font.png"],
+            sx: 20,
+            sy: 0,
+            sWidth: 5,
+            sHeight: 7,
+          },
+          down: {
+            img: imgs["img/card_font.png"],
+            sx: 20,
+            sy: 7,
+            sWidth: 5,
+            sHeight: 7,
+          }
+        },
+        "5": {
+          up: {
+            img: imgs["img/card_font.png"],
+            sx: 25,
+            sy: 0,
+            sWidth: 5,
+            sHeight: 7,
+          },
+          down: {
+            img: imgs["img/card_font.png"],
+            sx: 25,
+            sy: 7,
+            sWidth: 5,
+            sHeight: 7,
+          }
+        },
+        "6": {
+          up: {
+            img: imgs["img/card_font.png"],
+            sx: 30,
+            sy: 0,
+            sWidth: 5,
+            sHeight: 7,
+          },
+          down: {
+            img: imgs["img/card_font.png"],
+            sx: 30,
+            sy: 7,
+            sWidth: 5,
+            sHeight: 7,
+          }
+        },
+        "7": {
+          up: {
+            img: imgs["img/card_font.png"],
+            sx: 35,
+            sy: 0,
+            sWidth: 5,
+            sHeight: 7,
+          },
+          down: {
+            img: imgs["img/card_font.png"],
+            sx: 35,
+            sy: 7,
+            sWidth: 5,
+            sHeight: 7,
+          }
+        },
+        "8": {
+          up: {
+            img: imgs["img/card_font.png"],
+            sx: 40,
+            sy: 0,
+            sWidth: 5,
+            sHeight: 7,
+          },
+          down: {
+            img: imgs["img/card_font.png"],
+            sx: 40,
+            sy: 7,
+            sWidth: 5,
+            sHeight: 7,
+          }
+        },
+        "9": {
+          up: {
+            img: imgs["img/card_font.png"],
+            sx: 45,
+            sy: 0,
+            sWidth: 5,
+            sHeight: 7,
+          },
+          down: {
+            img: imgs["img/card_font.png"],
+            sx: 45,
+            sy: 7,
+            sWidth: 5,
+            sHeight: 7,
+          }
+        },
+        "A": {
+          up: {
+            img: imgs["img/card_font.png"],
+            sx: 50,
+            sy: 0,
+            sWidth: 5,
+            sHeight: 7,
+          },
+          down: {
+            img: imgs["img/card_font.png"],
+            sx: 50,
+            sy: 7,
+            sWidth: 5,
+            sHeight: 7,
+          }
+        },
+        "K": {
+          up: {
+            img: imgs["img/card_font.png"],
+            sx: 55,
+            sy: 0,
+            sWidth: 5,
+            sHeight: 7,
+          },
+          down: {
+            img: imgs["img/card_font.png"],
+            sx: 55,
+            sy: 7,
+            sWidth: 5,
+            sHeight: 7,
+          }
+        },
+        "Q": {
+          up: {
+            img: imgs["img/card_font.png"],
+            sx: 60,
+            sy: 0,
+            sWidth: 5,
+            sHeight: 7,
+          },
+          down: {
+            img: imgs["img/card_font.png"],
+            sx: 60,
+            sy: 7,
+            sWidth: 5,
+            sHeight: 7,
+          }
+        },
+        "J": {
+          up: {
+            img: imgs["img/card_font.png"],
+            sx: 65,
+            sy: 0,
+            sWidth: 5,
+            sHeight: 7,
+          },
+          down: {
+            img: imgs["img/card_font.png"],
+            sx: 65,
+            sy: 7,
+            sWidth: 5,
+            sHeight: 7,
+          }
+        },
+      },
+      layout: {
+        "2": {
+          up: [[42, 34]],
+          down: [[42, 100]]
+        },
+        "3": {
+          up: [[42, 34], [42, 67]],
+          down: [[42, 100]]
+        },
+        "4": {
+          up: [[20, 34], [64, 34]],
+          down: [[20, 100], [64, 100]],
+        },
+        "5": {
+          up: [[20, 34], [42, 67], [64, 34]],
+          down: [[20, 100], [64, 100]],
+        },
+        "6": {
+          up: [[20, 34], [20, 67], [64, 34], [64, 67]],
+          down: [[20, 100], [64, 100]],
+        },
+        "7": {
+          up: [[20, 34], [20, 67], [42, 50], [64, 34], [64, 67]],
+          down: [[20, 100], [64, 100]],
+        },
+        "8": {
+          up: [[20, 34], [20, 67], [42, 50], [64, 34], [64, 67]],
+          down: [[42, 84], [20, 100], [64, 100]],
+        },
+        "9": {
+          up: [[20, 34], [20, 57], [42, 67], [64, 34], [64, 56]],
+          down: [[20, 78], [20, 100], [64, 78], [64, 100]],
+        },
+        "10": {
+          up: [[20, 34], [20, 57], [42, 45], [64, 34], [64, 56]],
+          down: [[20, 78], [20, 100], [42, 89], [64, 78], [64, 100]],
+        },
+        "A": [42, 67],
+        "J": [17, 33],
+        "Q": [17, 33],
+        "K": [17, 33],
+      },
+      small: {
+        hearts: {
+          up: {
+            img: imgs["img/small_suits.png"],
+            sx: 0,
+            sy: 0,
+            sWidth: 12,
+            sHeight: 12,
+          },
+          down: {
+            img: imgs["img/small_suits.png"],
+            sx: 0,
+            sy: 12,
+            sWidth: 12,
+            sHeight: 12,
+          }
+        },
+        diamonds: {
+          up: {
+            img: imgs["img/small_suits.png"],
+            sx: 12,
+            sy: 0,
+            sWidth: 12,
+            sHeight: 12,
+          },
+          down: {
+            img: imgs["img/small_suits.png"],
+            sx: 12,
+            sy: 12,
+            sWidth: 12,
+            sHeight: 12,
+          }
+        },
+        clubs: {
+          up: {
+            img: imgs["img/small_suits.png"],
+            sx: 24,
+            sy: 0,
+            sWidth: 12,
+            sHeight: 12,
+          },
+          down: {
+            img: imgs["img/small_suits.png"],
+            sx: 24,
+            sy: 12,
+            sWidth: 12,
+            sHeight: 12,
+          }
+        },
+        spades: {
+          up: {
+            img: imgs["img/small_suits.png"],
+            sx: 36,
+            sy: 0,
+            sWidth: 12,
+            sHeight: 12,
+          },
+          down: {
+            img: imgs["img/small_suits.png"],
+            sx: 36,
+            sy: 12,
+            sWidth: 12,
+            sHeight: 12,
+          }
+        },
+      },
+      big: {
+        hearts: {
+          up: {
+            img: imgs["img/big_suits.png"],
+            sx: 0,
+            sy: 0,
+            sWidth: 16,
+            sHeight: 16,
+          },
+          down: {
+            img: imgs["img/big_suits.png"],
+            sx: 0,
+            sy: 16,
+            sWidth: 16,
+            sHeight: 16,
+          },
+          "A": {
+            img: imgs["img/big_suits.png"],
+            sx: 0,
+            sy: 0,
+            sWidth: 16,
+            sHeight: 16,
+          },
+          "J": {
+            img: imgs["img/royals.png"],
+            sx: 136,
+            sy: 0,
+            sWidth: 68,
+            sHeight: 86,
+          },
+          "Q": {
+            img: imgs["img/royals.png"],
+            sx: 136,
+            sy: 86,
+            sWidth: 68,
+            sHeight: 86,
+          },
+          "K": {
+            img: imgs["img/royals.png"],
+            sx: 136,
+            sy: 172,
+            sWidth: 68,
+            sHeight: 86,
+          }
+        },
+        diamonds: {
+          up: {
+            img: imgs["img/big_suits.png"],
+            sx: 16,
+            sy: 0,
+            sWidth: 16,
+            sHeight: 16,
+          },
+          down: {
+            img: imgs["img/big_suits.png"],
+            sx: 16,
+            sy: 16,
+            sWidth: 16,
+            sHeight: 16,
+          },
+          "A": {
+            img: imgs["img/big_suits.png"],
+            sx: 16,
+            sy: 0,
+            sWidth: 16,
+            sHeight: 16,
+          },
+          "J": {
+            img: imgs["img/royals.png"],
+            sx: 68,
+            sy: 0,
+            sWidth: 68,
+            sHeight: 86,
+          },
+          "Q": {
+            img: imgs["img/royals.png"],
+            sx: 68,
+            sy: 86,
+            sWidth: 68,
+            sHeight: 86,
+          },
+          "K": {
+            img: imgs["img/royals.png"],
+            sx: 68,
+            sy: 172,
+            sWidth: 68,
+            sHeight: 86,
+          }
+        },
+        clubs: {
+          up: {
+            img: imgs["img/big_suits.png"],
+            sx: 32,
+            sy: 0,
+            sWidth: 16,
+            sHeight: 16,
+          },
+          down: {
+            img: imgs["img/big_suits.png"],
+            sx: 32,
+            sy: 16,
+            sWidth: 16,
+            sHeight: 16,
+          },
+          "A": {
+            img: imgs["img/big_suits.png"],
+            sx: 32,
+            sy: 0,
+            sWidth: 16,
+            sHeight: 16,
+          },
+          "J": {
+            img: imgs["img/royals.png"],
+            sx: 0,
+            sy: 0,
+            sWidth: 68,
+            sHeight: 86,
+          },
+          "Q": {
+            img: imgs["img/royals.png"],
+            sx: 0,
+            sy: 86,
+            sWidth: 68,
+            sHeight: 86,
+          },
+          "K": {
+            img: imgs["img/royals.png"],
+            sx: 0,
+            sy: 172,
+            sWidth: 68,
+            sHeight: 86,
+          }
+        },
+        spades: {
+          up: {
+            img: imgs["img/big_suits.png"],
+            sx: 48,
+            sy: 0,
+            sWidth: 16,
+            sHeight: 16,
+          },
+          down: {
+            img: imgs["img/big_suits.png"],
+            sx: 48,
+            sy: 16,
+            sWidth: 16,
+            sHeight: 16,
+          },
+          "A": {
+            img: imgs["img/big_suits.png"],
+            sx: 48,
+            sy: 0,
+            sWidth: 16,
+            sHeight: 16,
+          },
+          "J": {
+            img: imgs["img/royals.png"],
+            sx: 204,
+            sy: 0,
+            sWidth: 68,
+            sHeight: 86,
+          },
+          "Q": {
+            img: imgs["img/royals.png"],
+            sx: 204,
+            sy: 86,
+            sWidth: 68,
+            sHeight: 86,
+          },
+          "K": {
+            img: imgs["img/royals.png"],
+            sx: 204,
+            sy: 172,
+            sWidth: 68,
+            sHeight: 86,
+          }
+        },
+      }
+    };
+
+    // set tabletop
+    tabletop = {
+      deck: undefined,
+      cards: {},
+      zindex: 0,
+    };
+
+    // set dialogue
+    tabletop.deck = new Deck();
+
+    // compile();
+  }
+}
